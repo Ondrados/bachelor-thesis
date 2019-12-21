2a9814b0391aee22a99188007be50dfbabd77a28
ondrados <ondrejdoskocil@gmail.com>
2019-12-21 19:31:57 +0100

    Dataset

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
 import os
 import glob
+from skimage.io import imread
+from matplotlib import pyplot as plt
+import numpy as np
 
 path_to_data='/Users/ondra/Dev/Personal/cnn-cells/data-science-bowl-2018'
 split='stage1_train'
@@ -9,8 +12,29 @@ id_list = os.listdir(path)
 image_list = []
 mask_list = []
 
-for id in self.id_list:
-    images = glob.glob(self.path + '/' + id + '/images/*png')
-    masks = glob.glob(self.path + '/' + id + '/masks/*png')
-    self.image_list.extend(images)
-    self.mask_list.extend(masks)
+for id in id_list:
+    images = glob.glob(path + '/' + id + '/images/*png')
+    masks = glob.glob(path + '/' + id + '/masks/*png')
+    image_list.extend(images)
+    mask_list.append(masks)
+
+image_path = image_list[0]
+masks_path = mask_list[0]
+print(image_path)
+image = imread(image_path)
+# comb_mask = np.zeros((256,256), dtype=float)
+
+comb_mask = None
+for path in masks_path:
+    mask = imread(path)
+    if comb_mask is None:
+        comb_mask = np.zeros_like(mask, dtype=np.float32())
+    comb_mask += mask
+
+fig = plt.figure(figsize=(1, 2))
+fig.add_subplot(1, 2, 1)
+plt.imshow(image)
+fig.add_subplot(1, 2, 2)
+plt.imshow(comb_mask, cmap='binary')
+
+plt.show(block=True)
--- dataset.py
+++ dataset.py
@@ -1,19 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-import glob
 import os
-from skimage.io import imread
+import glob
+import numpy as np
+import torch
 
+from skimage.io import imread
 from torch.utils.data import Dataset
 
 
-class DataLoader(Dataset):
+class MyDataset(Dataset):
     def __init__(self, split="stage1_train", path_to_data='/Users/ondra/Dev/Personal/cnn-cells/data-science-bowl-2018'):
         self.split = split
         self.path = path_to_data + '/' + split
@@ -22,7 +16,6 @@ class DataLoader(Dataset):
         self.image_list = []
         self.mask_list = []
 
-#       set_trace()
         for id in self.id_list:
             images = glob.glob(self.path + '/' + id + '/images/*png')
             masks = glob.glob(self.path + '/' + id + '/masks/*png')
@@ -33,15 +26,12 @@ class DataLoader(Dataset):
         return len(self.id_list)
 
     def __getitem__(self, index):
-        # zpracovat zde, centroidy
-        # set_trace()
         im = imread(self.image_list[index])
         msk = self.combine_masks(self.mask_list[index])
+        # pre process transforming to tensor
         image = torch.Tensor(im.astype(np.float32))
         mask = torch.Tensor(msk.astype(np.float32))
         return image, mask
-    # load and preprocess one image - with number index
-    # torchvision.transforms  contains several preprocessing functions for images
 
     def combine_masks(self, mask_paths):
         comb_mask = None
@@ -52,14 +42,6 @@ class DataLoader(Dataset):
             comb_mask += mask
         return comb_mask
 
-loader = DataLoader(split='stage1_train')
-trainloader = DataLoader(
-    loader, batch_size=2, num_workers=0, shuffle=True, drop_last=True)
-
-
-for it, (batch, masks) in enumerate(trainloader):  # you can iterate over dataset (one epoch)
-    print(batch)
-    print(batch.size())
-    print(masks)
-    plt.imshow(batch[0, :, :].detach().cpu().numpy())
-    break
+    def pre_process(self):
+        # pre processing
+        pass
