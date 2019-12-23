fe1c21f42c994b5e8c2a1b10c24d80b8f4600035
ondrados <ondrejdoskocil@gmail.com>
2019-12-23 15:18:17 +0100

    training update

--- train.py
+++ train.py
@@ -7,10 +7,10 @@ from torch.utils import data
 from dataset import MyDataset
 from unet import UNet
 
-batch = 1
+batch = 8
 dataset = MyDataset(split="stage1_train")
 trainloader = data.DataLoader(
-    dataset, batch_size=batch, num_workers=8, shuffle=True, drop_last=True,)
+    dataset, batch_size=batch, num_workers=1, shuffle=True, drop_last=True,)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -28,8 +28,6 @@ for epoch in range(50):  # loop over the dataset multiple times
     for i, data in enumerate(trainloader):
         # get the inputs; data is a list of [inputs, labels]
         inputs, masks = data[0].to(device=device), data[1].to(device=device)
-        inputs = inputs.unsqueeze(1)
-        masks = masks.unsqueeze(1)
 
         inputs.requires_grad = True
         masks.requires_grad = True
@@ -54,9 +52,16 @@ for epoch in range(50):  # loop over the dataset multiple times
 
     plt.plot(training_loss)
     plt.show()
-    plt.imshow(outputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
-    plt.show()
+
+    fig = plt.figure(figsize=(1, 3))
+    fig.add_subplot(1, 3, 1)
+    plt.imshow(inputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+    fig.add_subplot(1, 3, 2)
     plt.imshow(masks[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
-    plt.show()
+    fig.add_subplot(1, 3, 3)
+    plt.imshow(outputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+    plt.show(block=True)
+
     torch.save(net.state_dict(), '/home/ubmi/Documents/cnn-cells/cnn-cells/my_model.pt')
+
 print('Training finished!!!')
