2c971334f505538e28b24c0132f88cbb137a6510
ondrados <ondrejdoskocil@gmail.com>
2020-01-02 20:23:48 +0100

    testing detection

--- mask.py
+++ mask.py
@@ -1,4 +1,71 @@
+import torch
+import numpy as np
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 from dataset import MyDataset
+from unet import UNet
+from skimage.exposure import histogram
+from skimage.feature import peak_local_max, match_template
+from skimage.morphology import local_maxima, h_maxima, extrema
 
-image, mask = MyDataset(path_to_data = '/Users/ondra/Dev/Personal/cnn-cells/data-science-bowl-2018').__getitem__(1)
+from skimage.measure import label
+from skimage import data
+from skimage import color
+from skimage import exposure
+
+dataset = MyDataset(split="stage1_train",path_to_data = '/Users/ondra/Dev/Personal/cnn-cells/data-science-bowl-2018')
+
+data_loader = DataLoader(
+    dataset, batch_size=1, num_workers=1, shuffle=True, drop_last=True)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+net = UNet(n_channels=1, n_classes=1)
+net.load_state_dict(torch.load('/Users/ondra/Dev/Personal/cnn-cells/models/my_model2.pt',map_location=device))
+net.to(device)
+
+with torch.no_grad():
+    # get the inputs and masks
+    data = next(iter(data_loader))
+    inputs, masks = data[0].to(device=device), data[1].to(device=device)
+
+    net.eval()
+
+    outputs = net(inputs)
+out = outputs[0, 0, :, :].numpy()
+mask = masks[0, 0, :, :].numpy()
+array = np.where(out > 0.3, 1, 0)
+diff = mask-array
+fig = plt.figure()
+
+
+
+h = 0.3
+h_maxima = extrema.h_maxima(out, h)
+label_h_maxima = label(h_maxima)
+overlay_h = color.label2rgb(label_h_maxima, inputs[0, 0, :, :], alpha=0.7, bg_label=0,
+                            bg_color=None, colors=[(1, 0, 0)])
+
+coordinates = peak_local_max(label_h_maxima, indices=True)
+
+mask_h = 0.3
+mask_h_maxima = extrema.h_maxima(out, h)
+mask_label_h_maxima = label(h_maxima)
+overlay_h = color.label2rgb(label_h_maxima, inputs[0, 0, :, :], alpha=0.7, bg_label=0,
+                            bg_color=None, colors=[(1, 0, 0)])
+
+coordinates = peak_local_max(label_h_maxima, indices=True)
+# fig.add_subplot(1, 4, 1)
+# plt.imshow(inputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+# fig.add_subplot(1, 4, 2)
+# plt.imshow(outputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+# fig.add_subplot(1, 4, 3)
+# plt.imshow(array, cmap="gray")
+# fig.add_subplot(1, 4, 4)
+# plt.imshow(inputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+# plt.plot(coordinates[:,1],coordinates[:,0], 'r+')
+
+plt.imshow(inputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+plt.plot(coordinates[:,1],coordinates[:,0], 'r+', markersize=10)
+plt.show(block=True)
+# plt.savefig('images/train_output.png')
--- predict.py
+++ predict.py
@@ -1,5 +1,81 @@
 
 # pick one image from the test set
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import matplotlib
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, random_split
+
+from dataset import MyDataset
+from unet import UNet
+
+
+batch = 8
+dataset = MyDataset(split="stage1_train")
+trainset, valset = random_split(dataset, [500, 170])
+
+train_loader = DataLoader(
+    trainset, batch_size=batch, num_workers=1, shuffle=True, drop_last=True)
+val_loader = DataLoader(
+    valset, batch_size=batch, num_workers=1, shuffle=True, drop_last=True)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+net = UNet(n_channels=1, n_classes=1)
+net.to(device=device)
+
+criterion = nn.MSELoss()
+# criterion = nn.BCELoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+validation_loss = []
+
+def evaluate():
+    running_loss = 0.0
+    with torch.no_grad():
+        for i, data in enumerate(val_loader):
+            # get the inputs and masks
+            inputs, masks = data[0].to(device=device), data[1].to(device=device)
+
+            net.eval()
+
+            # only forward
+            outputs = net(inputs)
+            loss = criterion(outputs, masks)
+
+            # print statistics
+            print(f'{i}/{len(val_loader)}/{epoch}/eval')
+            running_loss += loss.item()
+        running_loss = running_loss / len(val_loader)
+        validation_loss.append(running_loss)
+
+        fig = plt.figure()
+        fig.add_subplot(1, 3, 1)
+        plt.imshow(inputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+        fig.add_subplot(1, 3, 2)
+        plt.imshow(masks[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+        fig.add_subplot(1, 3, 3)
+        plt.imshow(outputs[0, 0, :, :].detach().cpu().numpy(), cmap="gray")
+        # plt.show(block=True)
+        plt.savefig('images/eval_output.png')
+
+
+for epoch in range(50):  # loop over the dataset multiple times
+    evaluate()
+    fig = plt.figure()
+    plt.plot(training_loss, label="training_loss")
+    plt.plot(validation_loss, label="validation_loss")
+    plt.legend()
+    # plt.show()
+    plt.savefig('images/training_loss.png')
+    plt.close("all")
+    torch.save(net.state_dict(), '/home/ubmi/Documents/cnn-cells/cnn-cells/models/my_model.pt')
+
+print('Training finished!!!')
 img, _ = dataset_test[0]
 # put the model in evaluation mode
 model.eval()
