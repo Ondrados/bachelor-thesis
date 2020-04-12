1bf4ac54569913bd3e4bea4ba7480fd23a7c96b7
ondrados <ondrejdoskocil@gmail.com>
2020-04-12 17:06:36 +0200

    feat(fast_rcnn): added image saving

--- train_faster_rcnn.py
+++ train_faster_rcnn.py
@@ -23,6 +23,7 @@ optimizer = torch.optim.SGD(params, lr=0.01,
 dataset = MyDataset(split='stage1_train', transforms=get_transform(train=True))
 trainset, valset = random_split(dataset, [500, 170])
 
+training_loss = {}
 
 def train():
     i = 0
@@ -52,9 +53,19 @@ def evaluate():
         print(f"Epoch: {epoch}/{i} of {len(trainset)}, image {name} - eval")
         image.to(device=device)
         image = image[None, :, :, :]
-        predictions = model(image)
+        prediction = model(image)
 
-        # TODO: add image save
+        # every 10 images
+        if (i % 10) == 0:
+            image2 = Image.fromarray(image.detach().numpy()[0, 0, :, :])
+            if image2.mode != "RGB":
+                image2 = image2.convert("RGB")
+            draw = ImageDraw.Draw(image2)
+            for box, score in zip(prediction[0]["boxes"], prediction[0]["scores"]):
+                x0, y0, x1, y1 = box
+                draw.rectangle([(x0, y0), (x1, y1)], outline=(255, 0, 255))
+
+            image2.save(f"/images/{name}-{epoch}.png")
 
 
 for epoch in range(num_epoch):
