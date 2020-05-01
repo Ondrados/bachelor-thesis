b328176b8aab2381d4f339a3b05d306cf8c4ff36
ondrados <ondrejdoskocil@gmail.com>
2020-05-01 14:34:59 +0200

    fix(dataset): added Dataloader compatibility

--- dataset.py
+++ dataset.py
@@ -50,13 +50,13 @@ class MyDataset(Dataset):
         image = np.array(Image.open(self.image_list[index]), dtype=np.uint8)
         image = image[:, :, :3]  # remove alpha channel
         boxes, labels = self.mask_to_bbox(self.mask_list[index])
-        targets = [
+        targets = \
             {
                 'boxes': torch.FloatTensor(boxes),
                 'labels': torch.LongTensor(labels),
                 'name': self.id_list[index]
             }
-        ]
+
 
         if self.transforms is not None:
             image, targets = self.transforms(image, targets)
@@ -108,22 +108,37 @@ class MyDataset(Dataset):
 
 if __name__ == "__main__":
 
+    # def my_collate(batch):
+    #     image = batch[0]
+    #     target = [item[1] for item in batch]
+    #     return image, target
+
     def my_collate(batch):
-        image = batch[0]
+        data = [item[0] for item in batch]
         target = [item[1] for item in batch]
-        return image, target
+        return data, target
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Running on {device}")
 
     dataset = MyDataset(split='stage1_train', transforms=get_transform(train=True))
-    # trainloader = DataLoader(dataset, batch_size=1, num_workers=0, shuffle=True, drop_last=True, collate_fn=my_collate)
-    # it = iter(trainloader)
-    # image, targets = next(it)
-
-    image, targets = dataset[0]
-    image = image[None, :, :, :]
-
+    trainloader = DataLoader(dataset, batch_size=1, num_workers=0, shuffle=True, collate_fn=my_collate)
+    it = iter(trainloader)
+    image, targets = next(it)
+    print(image)
+    print(targets)
+    print("-----")
+    image, targets = dataset[1]
+    name = targets["name"]
+    # image = image[None, :, :, :]
+    image = image.to(device=device)
+    targets = [{
+        "boxes": targets["boxes"].to(device=device),
+        "labels": targets["labels"].to(device=device),
+        "name": targets["name"]
+    }]
+    print(image)
+    print(targets)
 
     image = Image.fromarray(image.numpy()[0, 0, :, :])
     if image.mode != "RGB":
@@ -133,4 +148,4 @@ if __name__ == "__main__":
         x0, y0, x1, y1 = box
         draw.rectangle([(x0, y0), (x1, y1)], outline=(255, 0, 255))
 
-    image.show(title=targets[0]["name"][0])
+    image.show(title=name)
