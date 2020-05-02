8a12e64b4b2bab28092c719936403a2b52770481
ondrados <ondrejdoskocil@gmail.com>
2020-05-02 22:15:07 +0200

    feat(utils): moved to utils folder

--- yolo_v3/models.py
+++ yolo_v3/models.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-from yolo_v3.utils import parse_model_config, build_targets, to_cpu
+from yolo_v3.utils.utils import parse_model_config, build_targets, to_cpu
 
 
 def create_modules(module_defs):
