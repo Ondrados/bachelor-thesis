22016ba81c9aa27ca6e619f0fb3be05a5d1db578
ondrados <ondrejdoskocil@gmail.com>
2020-05-17 12:52:03 +0200

    fix(faster_rcnn): added actual eval function

--- faster_rcnn/train.py
+++ faster_rcnn/train.py
@@ -49,23 +49,25 @@ def train():
 
 
 def evaluate():
-    model.eval()
+    running_loss_sum_eval = 0
     for i, (image, targets) in enumerate(eval_loader):
         name = targets[0]["name"]
         image = image[0].to(device=device)
+        targets = [{
+            "boxes": targets[0]["boxes"].to(device=device),
+            "labels": targets[0]["labels"].to(device=device),
+            "name": name
+        }]
 
-        predictions = model(image)
+        with torch.no_grad():
+            loss = model(image, targets)
+            loss_sum = sum(lss for lss in loss.values())
 
-        image_copy = Image.fromarray(image.cpu().numpy()[0, 0, :, :])
-        if image_copy.mode != "RGB":
-            image_copy = image_copy.convert("RGB")
-        draw = ImageDraw.Draw(image_copy)
-        for box, score in zip(predictions[0]["boxes"], predictions[0]["scores"]):
-            x0, y0, x1, y1 = box
-            draw.rectangle([(x0, y0), (x1, y1)], outline=(255, 0, 255))
-        image_copy.save(os.path.join(images_path, f"faster_rcnn/{attempt}/images/{name}-{epoch}.png"))
+        running_loss_sum_eval += loss_sum
 
-        print(f"Epoch: {epoch}, iteration: {i} of {len(evalset)}, image: {name}")
+        print(f"Eval: {epoch}, iteration: {i} of {len(eval_loader)}, loss: {loss_sum}, image: {name}")
+
+    eval_loss_sum.append(running_loss_sum_eval / len(eval_loader))
 
 
 def plot_losses():
@@ -82,6 +84,16 @@ def plot_losses():
     plt.ylabel('loss')
     plt.legend()
     plt.savefig(os.path.join(images_path, f"faster_rcnn/{attempt}/plots/training_loss_{attempt}.png"))
+    plt.close()
+
+    plt.figure(figsize=(16, 12), dpi=200)
+    plt.plot(training_loss_sum, 'r-', label="training_loss", )
+    plt.plot(eval_loss_sum, 'b-', label="validation_loss", )
+    plt.title("Training and validation loss")
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.savefig(os.path.join(images_path, f"faster_rcnn/{attempt}/plots/training_eval_loss_{attempt}.png"))
     plt.close()
 
 
@@ -109,19 +121,20 @@ if __name__ == "__main__":
 
     split = "stage1_train"
     dataset = MyDataset(split=split, transforms=get_transforms(train=True, rescale_size=(256, 256)))
-    trainset, evalset = random_split(dataset, [660, 10])  # this evalset is only for training progress demonstration
+    trainset, evalset = random_split(dataset, [600, 70])
 
     train_loader = DataLoader(trainset, batch_size=1, num_workers=0, shuffle=True, collate_fn=my_collate)
     eval_loader = DataLoader(evalset, batch_size=1, num_workers=0, shuffle=False, collate_fn=my_collate)
 
     training_loss_sum = []
+    eval_loss_sum = []
     rpn_cls_loss = []
     roi_cls_loss = []
     rpn_reg_loss = []
     roi_reg_loss = []
 
     for epoch in range(num_epoch):
-        train()
+        # train()
         evaluate()
         plot_losses()
 
