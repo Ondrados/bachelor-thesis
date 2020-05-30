38ed86f8a05821bf51e5ca5683e50b760e7f35b0
ondrados <ondrejdoskocil@gmail.com>
2020-05-30 09:43:54 +0200

    feat(yolo): added current model and time measure

--- yolo_v3/predict.py
+++ yolo_v3/predict.py
@@ -1,4 +1,5 @@
 import os
+import time
 import torch
 import numpy as np
 from PIL import Image, ImageDraw
@@ -16,6 +17,8 @@ models_path = os.path.join(BASE_DIR, "models")
 images_path = os.path.join(BASE_DIR, "images")
 
 if __name__ == "__main__":
+    attempt = 3
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Running on {device}...")
 
@@ -24,19 +27,21 @@ if __name__ == "__main__":
 
     dataset = MyTestDataset(split='stage1_test', transforms=get_test_transforms(rescale_size=(416, 416)))
 
-    test_loader = DataLoader(dataset, batch_size=1, num_workers=0, shuffle=True)
+    test_loader = DataLoader(dataset, batch_size=1, num_workers=0, shuffle=False)
 
     model.eval()
     for i, (image, targets) in enumerate(test_loader):
         image = image[0].to(device=device)
-        name = targets["name"]
-
+        name = targets["name"][0]
+        start_time = time.time()
         with torch.no_grad():
             outputs = model(image)
-
         outputs = non_max_suppression(outputs, conf_thres=0.5)
-
-        boxes = outputs[0][:, 0:4]
+        elapsed_time = time.time() - start_time
+        if outputs[0] is not None:
+            boxes = outputs[0][:, 0:4]
+        else:
+            continue
 
         image_copy = Image.fromarray(image.cpu().numpy()[0, 0, :, :])
         if image_copy.mode != "RGB":
@@ -45,5 +50,9 @@ if __name__ == "__main__":
         for box in boxes:
             x0, y0, x1, y1 = box
             draw.rectangle([(x0, y0), (x1, y1)], outline=(255, 0, 255))
-        image_copy.show()
-        break
+        # image_copy.show()
+        # image_copy.save(os.path.join(images_path, f"yolo_v3/{attempt}/images/{name}.png"))
+        print(f"{name}, time: {elapsed_time}")
+        plt.imshow(image_copy)
+        plt.show()
+
