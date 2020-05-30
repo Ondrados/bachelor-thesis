30a6052fa5655075befb8032ee98ea1074264dc4
ondrados <ondrejdoskocil@gmail.com>
2020-05-30 09:44:20 +0200

    feat(faster_rcnn): added current model and time measure

--- faster_rcnn/predict.py
+++ faster_rcnn/predict.py
@@ -1,6 +1,8 @@
 import os
+import time
 import torch
 from PIL import Image, ImageDraw
+from matplotlib import pyplot as plt
 from torch.utils.data import random_split, DataLoader
 
 from data_utils import MyTestDataset, get_test_transforms
@@ -14,29 +16,36 @@ images_path = os.path.join(BASE_DIR, "images")
 if __name__ == "__main__":
     from models import model
 
+    attempt = 7
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Running on {device}...")
 
-    model.load_state_dict(torch.load(os.path.join(models_path, "faster_rcnn_6.pt"), map_location=device))
+    model.load_state_dict(torch.load(os.path.join(models_path, "faster_rcnn_7_30.pt"), map_location=device))
 
     dataset = MyTestDataset(split='stage1_test', transforms=get_test_transforms(rescale_size=(256, 256)))
 
-    test_loader = DataLoader(dataset, batch_size=1, num_workers=0, shuffle=True)
+    test_loader = DataLoader(dataset, batch_size=1, num_workers=0, shuffle=False)
 
     model.eval()
     for i, (image, targets) in enumerate(test_loader):
         image = image[0].to(device=device)
-        name = targets["name"]
-
+        name = targets["name"][0]
+        start_time = time.time()
         with torch.no_grad():
             predictions = model(image)
-
+        elapsed_time = time.time() - start_time
         image_copy = Image.fromarray(image.cpu().numpy()[0, 0, :, :])
         if image_copy.mode != "RGB":
             image_copy = image_copy.convert("RGB")
         draw = ImageDraw.Draw(image_copy)
         for box, score in zip(predictions[0]["boxes"], predictions[0]["scores"]):
-            x0, y0, x1, y1 = box
-            draw.rectangle([(x0, y0), (x1, y1)], outline=(255, 0, 255))
-        image_copy.show()
-        break
+            if score > 0.5:
+                x0, y0, x1, y1 = box
+                draw.rectangle([(x0, y0), (x1, y1)], outline=(255, 0, 255))
+        # image_copy.show()
+        # image_copy.save(os.path.join(images_path, f"faster_rcnn/{attempt}/images/{name}.png"))
+        print(f"{name}, time: {elapsed_time}")
+        plt.imshow(image_copy)
+        plt.show()
+
